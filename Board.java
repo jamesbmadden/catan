3c5b38199838f1738bcfb620a110409d0550a1de
James Madden <jamesbmadden@gmail.com>
2022-06-07 11:07:01 -0700

    Add a default settlement that should be rendered

--- Board.java
+++ Board.java
@@ -39,7 +39,7 @@ public class Board {
 
   // which settlement spaces are already filled with settlements
   int[][] settlements = {
-      { 0, 0, 0, 0, 0, 0, 0 },
+      { 1, 0, 0, 0, 0, 0, 0 },
       { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
